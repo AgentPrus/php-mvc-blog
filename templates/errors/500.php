71775930418e8bf5f48aaab9a9fe04e6493d9cc3
Mike <mykyta.prus@hebronsoft.com>
2020-06-05 09:05:20 +0300

    Handle Exceptions

---
+++ templates/errors/500.php
@@ -0,0 +1,5 @@
+<div class="container text-center">
+    <h2 class="display-1">500</h2>
+    <p class="lead">A 500 Error occurs when something blows with our code</p>
+    <p class="text-muted"><?=$error ?></p>
+</div>
